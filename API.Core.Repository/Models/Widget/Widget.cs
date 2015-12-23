16b735d8f17cf27a8ddcf1c5a6a89971f5127d2b
William Thomas <will.thomas@inmerge.com>
2015-12-22 20:49:39 -0700

    widget class

---
+++ API.Core.Repository/Models/Widget/Widget.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using API.Core.Repository.Interfaces;
+using API.Core.Repository.Models.Base;
+
+namespace API.Core.Repository.Models.Widget
+{
+    public class Widget : BaseEntity, IIdentifier, IModifiedOn, ICreatedOn, IObjectWithState
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string Description { get; set; }
+        public State State { get; set; }
+    }
+}
